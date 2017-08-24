ef5f44f5bd6b479a21ae0b69b3a3a57e6c3a9315
Marcos de Benedicto <mbenedicto@gmail.com>
2017-08-24 16:29:24 -0300

    Create teste.sh

---
+++ teste.sh
@@ -0,0 +1,8 @@
+#RODA COLETA
+while true; do  ./curl_exec.sh www.ig.com.br; sleep 2; done
+
+#RODA ATUALIZACAO SITE
+#while true; do  ./calcula_disp.sh; sleep 2; done
+
+#MOSTRA INDEX.HTML ATUALIZADO
+#while true; do clear; cat index.html; sleep 3;done
